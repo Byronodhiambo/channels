d1e67387dab0c5f6c5370d03123349f2b86db38b
Byronodhiambo <byronjonnes25@gmail.com>
2021-09-24 17:33:51 +0300

    commit

--- channel/channel/settings.py
+++ channel/channel/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
+    'directmessage',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ASGI_APPLICATION = 'channel.asgi.application'
